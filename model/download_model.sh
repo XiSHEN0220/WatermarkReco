5436ec1e0c2a8820c359ead93843470ab1f94c54
XiSHEN0220 <shenxiluc@gmail.com>
2019-08-14 15:37:20 -0400

    model

---
+++ model/download_model.sh
@@ -0,0 +1,17 @@
+wget https://www.dropbox.com/s/oyu61ekf3c7d8ma/ChairsFinetuned918.pth?dl=0
+mv ChairsFinetuned918.pth?dl=0 ChairsFinetuned918.pth
+
+wget https://www.dropbox.com/s/0ni4g7yucz9csyu/EngravingFinetuned73.pth?dl=0
+mv EngravingFinetuned73.pth?dl=0 EngravingFinetuned73.pth 
+
+wget https://www.dropbox.com/s/qrxlo57ldzwulxd/net98_8.pth?dl=0
+mv net98_8.pth?dl=0 net98_8.pth
+
+wget https://www.dropbox.com/s/8y014skvu6n0lnb/resnet18ImageNet.pth?dl=0
+mv resnet18ImageNet.pth?dl=0 resnet18ImageNet.pth
+
+wget https://www.dropbox.com/s/w7zfq9dpamj24bf/ShoesFinetuned443.pth?dl=0
+mv ShoesFinetuned443.pth?dl=0 ShoesFinetuned443.pth
+
+wget https://www.dropbox.com/s/a5tc58y6zrrmvdw/SyntheticFinetuned77.pth?dl=0
+mv SyntheticFinetuned77.pth?dl=0 SyntheticFinetuned77.pth
