008f38a8d86d74edb3d33b267d9d0344d2db7287
XiSHEN0220 <shenxiluc@gmail.com>
2019-08-23 23:14:18 +0200

    rm

--- localMatching/demo_B_Finetune.sh
+++
@@ -1,3 +0,0 @@
-python localMatching.py --modelPth ../model/EngravingFinetuned73.pth --labelJson ../data/BTestEngravingRot.json --searchDir ../data/watermark/B_cross_domain_plus/val/ --queryDir ../data/watermark/B_cross_domain_plus/val/  --flip --saveRefFeat --featScaleBase 11 --scaleImgRef 22 #--outDir visEngravingPlus
-
-python localMatching.py --modelPth ../model/SyntheticFinetuned77.pth --labelJson ../data/BTestSyntheticRot.json --searchDir ../data/watermark/B_cross_domain_plus/val/ --queryDir ../data/watermark/B_cross_domain_plus/val/  --flip --saveRefFeat --featScaleBase 11 --scaleImgRef 22 #--outDir visEngravingPlus
