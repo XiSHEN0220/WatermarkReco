b48f47910932a5e5d9f751fde2e1d63de1c04d19
XiSHEN0220 <shenxiluc@gmail.com>
2019-08-23 16:44:11 +0200

    py + update file

--- model/download_model.sh
+++ model/download_model.sh
@@ -1,4 +1,4 @@
-wget https://www.dropbox.com/s/oyu61ekf3c7d8ma/ChairsFinetuned918.pth?dl=0
+wget https://www.dropbox.com/s/78crrzfdes28t2v/ChairsFinetuned918.pth?dl=0
 mv ChairsFinetuned918.pth?dl=0 ChairsFinetuned918.pth
 
 wget https://www.dropbox.com/s/0ni4g7yucz9csyu/EngravingFinetuned73.pth?dl=0
@@ -10,8 +10,8 @@ mv net98_8.pth?dl=0 net98_8.pth
 wget https://www.dropbox.com/s/8y014skvu6n0lnb/resnet18ImageNet.pth?dl=0
 mv resnet18ImageNet.pth?dl=0 resnet18ImageNet.pth
 
-wget https://www.dropbox.com/s/w7zfq9dpamj24bf/ShoesFinetuned443.pth?dl=0
-mv ShoesFinetuned443.pth?dl=0 ShoesFinetuned443.pth
+wget https://www.dropbox.com/s/m5scin30jhs1ete/ShoesFinetuned522.pth?dl=0
+mv ShoesFinetuned522.pth?dl=0 ShoesFinetuned522.pth
 
 wget https://www.dropbox.com/s/a5tc58y6zrrmvdw/SyntheticFinetuned77.pth?dl=0
 mv SyntheticFinetuned77.pth?dl=0 SyntheticFinetuned77.pth
