9e3903170329be5be243f9a90ad2b46ad60c5c68
XiSHEN0220 <shenxiluc@gmail.com>
2019-08-23 23:18:47 -0400

    b

--- localMatching/demo_B.sh
+++ localMatching/demo_B.sh
@@ -1,4 +1,7 @@
-python localMatching.py --labelJson ../data/BTestEngravingRot.json --searchDir ../data/watermark/B_cross_domain_plus/val/ --queryDir ../data/watermark/B_cross_domain_plus/val/  --flip --saveRefFeat #--outDir visEngravingPlus
+python localMatching.py --labelJson ../data/BTestEngravingRot.json --searchDir ../data/watermark/B_cross_domain_plus/val/ --queryDir ../data/watermark/B_cross_domain_plus/val/  --flip --saveRefFeat 
 
-python localMatching.py --labelJson ../data/BTestSyntheticRot.json --searchDir ../data/watermark/B_cross_domain_plus/val/ --queryDir ../data/watermark/B_cross_domain_plus/val/  --flip --saveRefFeat  #--outDir visEngravingPlus
+python localMatching.py --labelJson ../data/BTestSyntheticRot.json --searchDir ../data/watermark/B_cross_domain_plus/val/ --queryDir ../data/watermark/B_cross_domain_plus/val/  --flip --saveRefFeat 
 
+python localMatching.py --labelJson ../data/BTestEngravingRot.json --searchDir ../data/watermark/B_cross_domain_plus/val/ --queryDir ../data/watermark/B_cross_domain_plus/val/  --flip --saveRefFeat --modelPth ../model/EngravingFinetuned73.pth 
+
+python localMatching.py --labelJson ../data/BTestSyntheticRot.json --searchDir ../data/watermark/B_cross_domain_plus/val/ --queryDir ../data/watermark/B_cross_domain_plus/val/  --flip --saveRefFeat --modelPth ../model/SyntheticFinetuned83.pth
