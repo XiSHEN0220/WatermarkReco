8326e5fc142f9d145153d9a089c220e757af8840
XiSHEN0220 <shenxiluc@gmail.com>
2019-08-23 23:29:15 -0400

    briquet baseline

--- localMatching/demo_Briquet_Baseline.sh
+++ localMatching/demo_Briquet_Baseline.sh
@@ -1,15 +1,15 @@
-#python AvgPoolBriquet.py --outResJson AvgPoolBriquet.json --nbSaveRefFeat 1000
+python AvgPoolBriquet.py --outResJson AvgPoolBriquet.json --nbSaveRefFeat 1000
 
-#python ConcatBriquet.py --outResJson ConcatBriquet.json --nbSaveRefFeat 1000
+python ConcatBriquet.py --outResJson ConcatBriquet.json --nbSaveRefFeat 1000
 
-#python LocalSimiBriquet.py --outResJson LocalSimiBriquet.json --nbSaveRefFeat 1000
-
-
-
-#python LocalSimiBriquet.py --outResJson LocalSimiBriquet.json --nbSaveRefFeat 1000 --modelPth ../model/SyntheticFinetuned83.pth
+python LocalSimiBriquet.py --outResJson LocalSimiBriquet.json --nbSaveRefFeat 1000
 
 
-python AvgPoolBriquet.py --outResJson AvgPoolBriquet.json --nbSaveRefFeat 1000 --modelPth ../model/SyntheticFinetuned83.pth
+
+python LocalSimiBriquet.py --outResJson LocalSimiBriquetFinetune.json --nbSaveRefFeat 1000 --modelPth ../model/SyntheticFinetuned83.pth
 
 
-python ConcatBriquet.py --outResJson ConcatBriquet.json --nbSaveRefFeat 1000 --modelPth ../model/SyntheticFinetuned83.pth
+python AvgPoolBriquet.py --outResJson AvgPoolBriquetFinetune.json --nbSaveRefFeat 1000 --modelPth ../model/SyntheticFinetuned83.pth
+
+
+python ConcatBriquet.py --outResJson ConcatBriquetFinetune.json --nbSaveRefFeat 1000 --modelPth ../model/SyntheticFinetuned83.pth
